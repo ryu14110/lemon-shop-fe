9a52e0bc3306bd4349cbcdb12ed6dc508ca586b5
yeojin.kim <yeojin.kim@yanolja.com>
2023-10-11 18:58:04 +0900

    feat: apiClient 모듈 추가

---
+++ src/utils/apiClient.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+axios.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const apiClient = async ({
+  url,
+  method = "GET",
+  data = null,
+  params = null,
+  baseURL = "http://localhost:4000",
+  headers,
+  config,
+  withCredentials = true,
+}) => {
+  const result = await axios({
+    method,
+    url,
+    data,
+    params,
+    baseURL,
+    headers,
+    withCredentials,
+    timeout: 30000,
+    ...config,
+  });
+
+  return result.data;
+};
