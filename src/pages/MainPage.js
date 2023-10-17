e77db44f236486c815680de14941b5980e6c4cf8
Gyeongjin Lee <asdfg031897@gmail.com>
2023-10-17 07:51:47 -0700

    add eslintignore.file

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,11 @@
 import ProductList from '../components/ProductList';
 import MainBar from '../components/MainBar';
 import Footer from '../components/Footer';
-import Sidebar from '../components/Sidebar';
 
 function App() {
   return (
     <>
       <MainBar />
-      <Sidebar />
       <ProductList />
       <Footer />
     </>
--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
-const db = require("./config.db");
+const db = require('./config.db');
 
-app.get("/", (req, res) => {
-  console.log("root");
+app.get('/', (req, res) => {
+  console.log('root');
 });
 
-app.get("/movies", (req, res) => {
-  console.log("/movies");
+app.get('/movies', (req, res) => {
+  console.log('/movies');
 });
 
 app.listen(PORT, () => {
   console.log(`Server on: http://localhost:${PORT}`);
 });
+
+//app.get(json)
