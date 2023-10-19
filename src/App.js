674f28937efce22647f894b7c734154c8ebb2f1e
daegyun cho <leonaryedo@gmail.com>
2023-10-19 16:44:38 +0900

    데이터 이동 설정하기

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ import RegisterPage from './pages/RegisterPage';
 import MainPage from './pages/MainPage';
 
 const App = () => {
+  const selectAll = async () => {
+    //async와 await로 비동기 지원
+    alert("selectAll!");
+    const result = await axios.get("/user");
+    //http://localhost:4000/user인 서버에 데이터를 요청한다. 이 설정은 package.json의 proxy설정에 있음
+    //요청한 데이터가 도착하면 result에 담는다.
+    console.log(result);
+  };
+
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      <Route path="/" element={<MainPage data={result} />} />
       <Route path="/items/:itemId" element={<ItemsPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
