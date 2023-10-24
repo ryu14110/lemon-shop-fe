085314fdc86d2389df522bae99f769b605eda1a9
daegyun cho <leonaryedo@gmail.com>
2023-10-24 21:34:17 +0900

    password 보안

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
-const db = require('./config/db.js');
+const db = require('./config/database.js');
 //const cors = require('cors');
 
 //app.use(cors());
