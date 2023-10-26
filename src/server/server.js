f8849a4b0c5a95f43bf9358f61294191c9269a83
brown <brown@brownui-MacBookPro.local>
2023-10-26 13:10:04 +0900

    fist commit

--- src/server/server.js
+++ src/server/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.static('public'));
 //cors해결을 위한 코드
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // 클라이언트 도메인
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4000'); // 클라이언트 도메인
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Credentials', 'true');
